4d5c0d8003d9a5123649f7cb2526a01f3cc5de50
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2019-07-11 12:48:02 +0200

    Controllo singola istanza applicazione

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 from ExperienceSampling.App import App
-import sys, os
+import sys, os, appdirs
+
+lockfolder = appdirs.user_data_dir('ExperienceSampling', 'UniBA')
+lockfile = os.path.join(lockfolder,'lockfile')
+
+if not os.path.exists(lockfolder):
+    os.makedirs(lockfolder)
 
 try:
+    if os.path.exists(lockfile):
+        os.unlink(lockfile)
+
+    fd =  os.open(lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
+
     with open("timer.txt", 'r') as file:
         timer = file.read()
     timer = int(timer)
     app = App(pollTime=timer)
-except:
+except OSError:
+    raise "Application already running."
+except (ValueError, FileNotFoundError):
     app = App()
 
 sys.exit(app.exec_())
