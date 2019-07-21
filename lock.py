98ab5bfaa7635c2cdb81797692452e594528914d
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2019-07-22 00:54:36 +0200

    Implementato file di lock per sistemi Unix

---
+++ lock.py
@@ -0,0 +1,28 @@
+import os, platform
+
+def lock(LOCK_PATH):
+    
+    is_running = False
+
+    if os.name == 'nt' or platform.system() == 'Windows' or 'cygwin' in platform.system().lower():
+        try:
+            if os.path.exists(LOCK_PATH):
+                os.unlink(LOCK_PATH)
+            fh = os.open(LOCK_PATH, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+        except EnvironmentError as err:
+            if err.errno == 13:
+                is_running = True
+            else:
+                raise
+    else:
+        import fcntl
+        try:
+            fh = open(LOCK_PATH, 'w')
+            fcntl.lockf(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except EnvironmentError as err:
+            if fh is not None:
+                is_running = True
+            else:
+                raise 
+
+    return is_running
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from ExperienceSampling.App import App
 import sys, os, appdirs
 from PyQt5.QtWidgets import QApplication, QErrorMessage
+from lock import lock
 
 lockfolder = appdirs.user_data_dir('ExperienceSampling', 'UniBA')
 lockfile = os.path.join(lockfolder,'lockfile')
@@ -8,15 +9,11 @@ lockfile = os.path.join(lockfolder,'lockfile')
 if not os.path.exists(lockfolder):
     os.makedirs(lockfolder)
 
-try:
-    if os.path.exists(lockfile):
-        os.unlink(lockfile)
-    fd =  os.open(lockfile, os.O_CREAT|os.O_EXCL|os.O_WRONLY)
-except OSError as e:
-        app = QApplication([])
-        error_dialog = QErrorMessage()
-        error_dialog.showMessage("Application already running!")
-        sys.exit(app.exec_())
+if lock(lockfile):
+    app = QApplication([])
+    error_dialog = QErrorMessage()
+    error_dialog.showMessage("Application already running!")
+    sys.exit(app.exec_())
 
 try:
     with open("timer.txt", 'r') as file:
