8431fba763140a8b4794510465ba5b7eb4372489
Giuseppe Antonio Nanna <g.a.nanna@inventati.org>
2019-07-22 14:08:18 +0200

    Bugfix: file di lock non funzionante in Linux

--- lock.py
+++ lock.py
@@ -1,15 +1,16 @@
-import os, platform
+import os, platform, time
 
 def lock(LOCK_PATH):
     """Returns true if other instances are running. This function uses a lock file."""
     
     is_running = False
+    global FH
 
     if os.name == 'nt' or platform.system() == 'Windows' or 'cygwin' in platform.system().lower():
         try:
             if os.path.exists(LOCK_PATH):
                 os.unlink(LOCK_PATH)
-            fh = os.open(LOCK_PATH, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+            FH = os.open(LOCK_PATH, os.O_CREAT | os.O_EXCL | os.O_RDWR)
         except EnvironmentError as err:
             if err.errno == 13:
                 is_running = True
@@ -18,10 +19,10 @@ def lock(LOCK_PATH):
     else:
         import fcntl
         try:
-            fh = open(LOCK_PATH, 'w')
-            fcntl.lockf(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            FH = open(LOCK_PATH, 'w')
+            fcntl.lockf(FH, fcntl.LOCK_EX | fcntl.LOCK_NB)
         except EnvironmentError as err:
-            if fh is not None:
+            if FH is not None:
                 is_running = True
             else:
                 raise 
